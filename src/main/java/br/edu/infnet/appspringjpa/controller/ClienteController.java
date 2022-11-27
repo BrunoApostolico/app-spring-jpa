d4c39cb46a68931e48eb10036d69e9aa4785ae88
Bruno Apostolico <bapostolico@gmail.com>
2022-11-27 11:07:05 -0300

    CRUD de clientes

--- src/main/java/br/edu/infnet/appspringjpa/controller/ClienteController.java
+++ src/main/java/br/edu/infnet/appspringjpa/controller/ClienteController.java
@@ -1,9 +1,13 @@
 package br.edu.infnet.appspringjpa.controller;
 
+import br.edu.infnet.appspringjpa.entity.Cliente;
 import br.edu.infnet.appspringjpa.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
@@ -11,4 +15,44 @@ public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Cliente cadastrar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listar(){
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente buscarPorId(@PathVariable Long id){
+        var clienteOptional = clienteRepository.findById(id);
+        if (clienteOptional.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return clienteOptional.get();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void excluirPorId(@PathVariable Long id){
+        var clienteOptional = clienteRepository.findById(id);
+        if (clienteOptional.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        clienteRepository.delete(clienteOptional.get());
+    }
+
+    @PutMapping("/{id}")
+    public Cliente atualizarPorId(@PathVariable Long id, @RequestBody Cliente cliente){
+        var clienteOptional = clienteRepository.findById(id);
+        if (clienteOptional.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        cliente.setId(id);
+        return clienteRepository.save(cliente);
+
+    }
 }
